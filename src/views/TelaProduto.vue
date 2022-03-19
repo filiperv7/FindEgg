84ecc96a64599a62fde8fe3cddf65660dd77ea0d
Pedro Henrick <pedrohenrickqa@gmail.com>
2022-03-19 17:13:47 -0300

    conclui a tela de produto

--- src/views/TelaProduto.vue
+++ src/views/TelaProduto.vue
@@ -3,7 +3,19 @@
     <!-- Pedro -->
     <v-container class="mb-16">
       <v-img :src="produto.imagem"></v-img>
-      <h1 class="text-h5 text--primary">{{ produto.nome }}</h1>
+      <div id="preco-nome">
+        <h1 class="text-h5 text--primary">{{ produto.nome }}</h1>
+        <p>R$ {{ produto.preco }}</p>
+      </div>
+      <div id="subtitle">
+        <p>{{ produto.sabor }}</p>
+      </div>
+      <div>
+        <h2 class="text-h6">Dados do vendedor:</h2>
+        <p class="mb-0">{{ produto.local.nome }}</p>
+        <p class="mb-0">CEP: {{ produto.local.cep }}</p>
+        <p>Endereço: {{ produto.local.endereco }}</p>
+      </div>
     </v-container>
   </v-app>
 </template>
@@ -36,4 +48,12 @@ export default {
 </script>
 
 <style scoped>
+.preco-nome > p {
+  display: flex;
+}
+
+.subtitle > p {
+  margin-top: 10px;
+  color: rgb(177, 177, 177);
+}
 </style>

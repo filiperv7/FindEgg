7f6aab78fb536c5eae069120cb87ebe62d9501cc
Antonieta Naomi Bazan <bazan.antonieta@gmail.com>
2022-03-19 14:45:09 -0300

    alterações na bottom

--- src/components/shared/BottomBar.vue
+++ src/components/shared/BottomBar.vue
@@ -1,63 +1,61 @@
 <template>
   <div class="bottom-bar">
     <!-- Naomi -->
-      <v-bottom-navigation
-    :value="value"
-    color="teal"
-    active-class= "white"
-     grow
-  >
-    <v-btn class="botoes">
+    <v-bottom-navigation :value="value" color="teal" active-class="white" grow>
+  
       <router-link to="/">
-        <span>Início</span>
-      </router-link>
-
-      <v-icon>mdi-home</v-icon>
-    </v-btn>
-
-    <v-btn class="botoes">
-      <router-link to="/sobrenos">
-        <span>Sobre nós</span>
-      </router-link>
-
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-
         <v-btn class="botoes">
-      <router-link to="/fornecedores">
+          <span>Início</span>
+
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+      </router-link>
+
+    <router-link to="/sobrenos">
+      <v-btn class="botoes">
+          <span>Sobre nós</span>
+        
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </router-link>
+
+    <router-link to="/produtos">
+      <v-btn class="botoes">
         <span>Ovos</span>
-      </router-link>
+        
 
-      <v-icon>mdi-candy</v-icon>
-    </v-btn>
+        <v-icon>mdi-candy</v-icon>
+      </v-btn>
+    </router-link>
 
-    <v-btn class="botoes">
-      <router-link to="/login">
+    <router-link to="/login">
+      <v-btn class="botoes">
         <span>Login</span>
-      </router-link>
-
-      <v-icon>mdi-account</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
+        
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+    </router-link>
+    </v-bottom-navigation>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'BottomBar',
-    data: () => ({ value: 0 }),
-  }
+export default {
+  name: "BottomBar",
+  data: () => ({ value: 0 }),
+};
 </script>
 
 <style scoped>
-  .botoes {
-    background-color: none;
-  }
-  .bottom-bar a{
-    color: black;
-    text-decoration: none;
-    display:flex;
-    justify-content: center;
-    flex-grow: 1;
+.botoes {
+  background-color: none;
+  color: black;
+}
+.bottom-bar a {
+  color: blue-lighten-1;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  flex-grow: 2;
 }
 </style>
